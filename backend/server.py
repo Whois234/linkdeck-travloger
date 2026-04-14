67bb03582fe287a6911f2e7b407009d32d5158e8
Gajanand G <gajanand@Gajanands-MacBook-Air.local>
2026-04-14 23:20:49 +0530

    fix cors for vercel

--- backend/server.py
+++ backend/server.py
@@ -368,7 +368,10 @@ frontend_url = os.environ.get("FRONTEND_URL", "http://localhost:3000")
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=[frontend_url, "http://localhost:3000"],
+    allow_origins=[
+     "http://localhost:3000",
+     "https://linkdeck-travloger.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
